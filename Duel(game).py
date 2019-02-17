fa77e9ff33410d45667a8907e06b5474d5ac1831
Tagir <tagir.kabirov.02@mail.ru>
2019-02-17 22:28:29 +0300

    The end result of the game

--- Duel(game).py
+++ Duel(game).py
@@ -4,11 +4,15 @@ pygame.init()
 win = pygame.display.set_mode((900, 600))
 
 clock = pygame.time.Clock()
+START = True
+END = False
+run = True
+SCORE = '0'
 
 pygame.display.set_caption("Duel")
 cloud = pygame.image.load('sprites/cloud.png')
-
 opponent = pygame.image.load('sprites/opponent.png')
+picture = pygame.image.load('sprites/start.jpg')
 
 walkRight = [pygame.image.load('sprites/anim_r_1.png'),
              pygame.image.load('sprites/anim_r_2.png'),
@@ -28,6 +32,8 @@ walkLeft = [pygame.image.load('sprites/anim_l_1.png'),
 
 bg = pygame.image.load('sprites/bg.jpg')
 playerStand = pygame.image.load('sprites/anim_stay.png')
+end = pygame.image.load('sprites/end.jpg')
+
 
 x = 50
 y = 252
@@ -42,11 +48,41 @@ left = False
 right = False
 anim = 0
 
+
+class BULLET():
+    def __init__(self, x, y,  rad):
+        self.x = x
+        self.y = y
+        self.rad = rad
+        self.speed = 10
+
+    def draw(self):
+        pygame.draw.circle(win, (0, 0, 0),  (self.x, self.y), self.rad)
+
+
+bulet = BULLET(round(700), round(360), 5)
+
+
+def shooting():
+    global SCORE
+    if bulet.x > 0:
+        bulet.draw()
+    else:
+        SCORE = str(int(SCORE)+1)
+        bulet.x = 700
+        bulet.y = 360
+
+
+
 def draw():
     global anim
+    global START
+    global run
+
     win.blit(bg, (0, 0))
     win.blit(cloud, (50, 400))
     win.blit(cloud, (550, 400))
+    shooting()
     win.blit(opponent, (700, 305))
     if anim + 1 > 30:
         anim = 0
@@ -58,11 +94,17 @@ def draw():
         anim += 1
     else:
         win.blit(playerStand, (x, y))
-
+    font = pygame.font.Font(None, 50)
+    text = font.render("SCORE : " + SCORE, 1, (255, 255, 255))
+    win.blit(text, (370, 150))
+    if START:
+        win.blit(picture, (0, 0))
+    if END:
+        win.blit(end, (0, 0))
+        run = False
     pygame.display.update()
 
 
-run = True
 while run:
     clock.tick(30)
 
@@ -70,6 +112,13 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
+    bulet.x -= bulet.speed
+    if SCORE == '10':
+        bulet.speed = 20
+    elif SCORE == '20':
+        bulet.speed = 25
+    elif SCORE == '30':
+        bulet.speed = 30
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and x > 30 and keys[pygame.K_RIGHT] == 0:
         x -= speed
@@ -96,4 +145,9 @@ while run:
             isJump = False
             jumpCount = 10
     draw()
+    if x < bulet.x < x + width and y < bulet.y < y + height:
+        END = True
+    if START:
+        pygame.time.delay(5000)
+        START = False
 pygame.quit()
